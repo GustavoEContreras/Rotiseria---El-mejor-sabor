4673a9d6d0b3111a1f9729022b417f960dd4cb4d
GustavoEContreras <gustavocon1@hotmail.com>
2022-09-15 15:20:36 -0300

    Corregido codigo js de realizarPedido, funcionalidad horario

--- archivos js/realizarPedido.js
+++ archivos js/realizarPedido.js
@@ -25,92 +25,7 @@ else
     diaActual = diaActual.toString();
 }
 inputFechaEntrega.value = anioActual.toString() + "-" + mesActual + "-" + diaActual;
-inputFechaEntrega.setAttribute('min', anioActual.toString() + "-" + mesActual + "-" + diaActual);
-
-
-/*
- Verificar que el horario para la entrega del pedido, sea un horario en el que el local se
-encuentra trabajando.
-
-
-const inputHoraDesde = document.getElementById('horaDesde');
-const inputMinutoDesde = document.getElementById('minutoDesde');
-
-
-function verificarHorario()
-{
-    let horaDesde = parseInt(inputHoraDesde.value,10);
-    let minutoDesde = parseInt(inputMinutoDesde.value,10);
-    if (horaDesde < 10)
-    {
-        alert('El pedido no puede ser entregado fuera del horario de la rotiseria');
-        inputHoraDesde.value = 10;
-    }
-    else if (horaDesde >= 22)
-    {
-        alert('El pedido no puede ser entregado fuera del horario de la rotiseria');
-        inputHoraDesde.value = 21;
-    }
-
-    if (minutoDesde < 0)
-    {
-        inputMinutoDesde.value = 55;
-    }
-    else if (minutoDesde > 59)
-    {   
-        inputMinutoDesde.value = 0;
-        
-    }
-
-    if (horaDesde === 21 && minutoDesde > 30)
-    {
-        alert('El pedido no puede ser entregado fuera del horario de la rotiseria');
-        inputMinutoDesde.value = 30;
-    }
-    
-}
-
-const inputHorarioHasta = document.getElementById("horariosHasta");
-
-function actualizarHorarioHasta()
-{
-    let horaHasta = parseInt(inputHoraDesde.value,10);
-    let minutoHasta = parseInt(inputMinutoDesde.value,10) + 30;
-    if (minutoHasta >= 60)
-    {
-        minutoHasta -= 60;
-        if (minutoHasta === 0)
-        {
-            minutoHasta = "00";
-        }
-        else if (minutoHasta === 5)
-        {
-            minutoHasta = "05";
-        }
-        else
-        {
-            minutoHasta = minutoHasta.toString();
-        }
-        horaHasta += 1;
-        horaHasta = horaHasta.toString();
-        inputHorarioHasta.value = String.prototype.concat(horaHasta,":",minutoHasta);
-    }
-    else{
-        inputHorarioHasta.value = String.prototype.concat(horaHasta.toString(),":",minutoHasta.toString());
-    }
-}
-inputHoraDesde.addEventListener('change', verificarHorario);
-inputHoraDesde.addEventListener('onkeyup', verificarHorario);
-inputHoraDesde.addEventListener('onkeydown', verificarHorario);
-
-inputMinutoDesde.addEventListener('change', verificarHorario);
-inputMinutoDesde.addEventListener('onkeyup', verificarHorario);
-inputMinutoDesde.addEventListener('onkeydown', verificarHorario);
-// inputMinutoDesde.addEventListener('change', verificarHorario);
-
-inputHoraDesde.addEventListener('change', actualizarHorarioHasta);
-inputMinutoDesde.addEventListener('change', actualizarHorarioHasta);
-*/
+inputFechaEntrega.setAttribute('min', anioActual.toString() + "-" + mesActual + "-" + diaActual); //  Se indica la fecha minima del pedido (la fecha actual)
 
 // 10:00AM -- 14:00PM                   Horarios donde no se puede iniciar un pedido:   13:31PM -- 20:29 PM
 // 20:30 PM -- 01:00AM                                                                  00:31AM -- 09:59AM
@@ -118,10 +33,11 @@ const botonRealizarPedido = document.getElementById('submitButton');
 const inputHoraDesde = document.getElementById('horaDesde');
 const inputMinutoDesde = document.getElementById('minutoDesde');
 
-function verificarHorario(event)
+function verificarHorario(event)    // El horario del pedido debe ser correcto, caso contrario, un cartel es indicado y se cancela el intento de realizar el pedido
 {
     let horaDesde = parseInt(inputHoraDesde.value,10);
-    
+    let minutoDesde = parseInt(inputMinutoDesde.value,10);
+
     if (
         ((horaDesde > 13) ||  ((horaDesde === 13) && (minutoDesde > 30))) && 
         ((horaDesde < 20) || ((horaDesde === 20) && (minutoDesde <= 30))) 
@@ -131,9 +47,9 @@ function verificarHorario(event)
         event.preventDefault();
     }
     else if (
-        ((horaDesde >= 1) || (((horaDesde === 0) && (minutoDesde > 30)) &&
-        ((horaDesde < 10))))
-    )
+        ((horaDesde >= 1) || (((horaDesde === 0) && (minutoDesde > 30)))) &&
+        ((horaDesde < 10))
+        )
     {
         alert('El pedido no puede ser entregado fuera del horario de la rotiseria');
         event.preventDefault();
@@ -145,7 +61,7 @@ function verificarHorario(event)
 botonRealizarPedido.addEventListener('click', verificarHorario);
 
 
-function verificarMinutos()
+function verificarMinutos() // Los minutos del horario de pedido deben ser entre 0 y 60
 {
     let minutoDesde = parseInt(inputMinutoDesde.value,10);
     if (minutoDesde < 0)
@@ -165,7 +81,7 @@ inputMinutoDesde.addEventListener('onkeydown', verificarMinutos);
 
 const inputHorarioHasta = document.getElementById("horariosHasta");
 
-function actualizarHorarioHasta()
+function actualizarHorarioHasta()   // El horario "hasta" debe ser hasta 30 minutos despues del horario "desde"
 {
     let horaHasta = parseInt(inputHoraDesde.value,10);
     let minutoHasta = parseInt(inputMinutoDesde.value,10) + 30;
@@ -195,9 +111,52 @@ function actualizarHorarioHasta()
 
 inputHoraDesde.addEventListener('change', actualizarHorarioHasta);
 inputMinutoDesde.addEventListener('change', actualizarHorarioHasta);
+
 /*
  Una vez finalizada la orden de pedido, se debe realizar el cálculo del total en forma automática, y ese campo debe estar deshabilitado.
 */
+
+class Producto { //Clase Producto
+    constructor(nombre, precio, cantidad)
+    {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+    }
+}
+
+let producto1 = new Producto("Tarta de anquin y queso", 700, 1);
+let producto2 = new Producto("Bifes de pollo", 400, 2);
+
+let listaProductos = [];
+listaProductos.push(producto1);
+listaProductos.push(producto2);
+
+const divListaProductos = document.getElementById("listaProductos");
+for (let producto of listaProductos) {
+    const divPlato = document.createElement("div");
+    divPlato.setAttribute("class", "plato");
+    divPlato.innerHTML = `
+        <div class="row">
+            <div class="col-2"></div>
+            <div class="col-4">
+                <div class="text-center">
+                    <p><b>${producto.nombre}</b></p>
+                </div>
+            </div>
+            <div class="col-1">
+                <input type="number" value="${producto.precio * producto.cantidad}" readonly class="precioProducto">
+            </div>
+        </div>
+    `
+    divListaProductos.appendChild(divPlato);
+}
+
+
+
+
+
+
 const productos = document.body.getElementsByClassName('precioProducto');
 let precioProductos = 0;
 for (const producto of productos) {
@@ -206,15 +165,42 @@ for (const producto of productos) {
 const inputPrecioProductos = document.getElementById('precioProductos');
 inputPrecioProductos.setAttribute('value', precioProductos);
 
+const inputTipoEntregaRetiro = document.getElementById("retiro");
+const inputTipoEntregaDelivery = document.getElementById("delivery");
 const inputEnvio = document.getElementById('envio');
-const precioEnvio = parseInt(inputEnvio.getAttribute('value'),10);
 
-const precioTotal = precioProductos + precioEnvio;
+function actualizarPrecioEnvio()
+{
+    if (inputTipoEntregaRetiro.checked)
+{
+    inputEnvio.setAttribute("value", 0);
+}
+else if (inputTipoEntregaDelivery.checked)
+{
+    inputEnvio.setAttribute("value",50);
+}
+}
 
-const inputPrecioTotal = document.getElementById('precioTotal');
-inputPrecioTotal.setAttribute('value', precioTotal);
+inputTipoEntregaRetiro.addEventListener("change", actualizarPrecioEnvio);
+inputTipoEntregaDelivery.addEventListener("change", actualizarPrecioEnvio);
 
 
+
+function actualizarPrecioTotal()
+{
+    const precioEnvio = parseInt(inputEnvio.getAttribute('value'),10);
+
+    const precioTotal = precioProductos + precioEnvio;
+
+    const inputPrecioTotal = document.getElementById('precioTotal');
+    inputPrecioTotal.setAttribute('value', precioTotal);
+}
+
+inputTipoEntregaRetiro.addEventListener("change", actualizarPrecioTotal);
+inputTipoEntregaDelivery.addEventListener("change", actualizarPrecioTotal);
+
+actualizarPrecioEnvio();
+actualizarPrecioTotal();
 /*
  En el caso de que se cambie el estado del pedido a “devuelto” o “cancelado”, requerir al
 usuario que ingrese un comentario.
