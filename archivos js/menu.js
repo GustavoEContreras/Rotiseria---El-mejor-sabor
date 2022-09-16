98abdba186946d4b37ad3b4820b463e9b8a2a065
GustavoEContreras <gustavocon1@hotmail.com>
2022-09-16 17:38:56 -0300

    Cambios en css

--- archivos js/menu.js
+++ archivos js/menu.js
@@ -1,2 +1,10 @@
-const ulListaEntradas = document.getElementById("listaPostres");
+const ulListaEntradas = document.getElementById("listaEntradas");
 const ulListaPlatosPrincipales = document.getElementById("listaPlatosPrincipales");
+const ulListaPostres = document.getElementById("listaPostres");
+
+
+
+class Plato
+{
+
+}
