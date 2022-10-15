538a9d7dd70c09a97fbd0475b17a243d892f58e9
GustavoEContreras <gustavocon1@hotmail.com>
2022-10-15 12:48:23 -0300

    Creada migracion para app Administrador

--- rotiseria/Administrador/models.py
+++ rotiseria/Administrador/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Administrador(models.Model):
+    cuil = models.DecimalField(max_digits=8, decimal_places=0, unique=True)
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+
+    MASCULINO = "M"
+    FEMENINO = "F"
+    SEXO_OPCIONES = (
+        (MASCULINO, "M"),
+        (FEMENINO, "F"),
+    )
+
+    sexo = models.CharField(max_length=1,
+                      choices=SEXO_OPCIONES,
+                      default=FEMENINO)
+
